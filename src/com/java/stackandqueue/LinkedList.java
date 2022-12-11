97b39ddcd9a7eea022d4ac754d8572928e78fcd3
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-12-11 20:46:15 +0530

    Added:Logic for creating the stack of push method

---
+++ src/com/java/stackandqueue/LinkedList.java
@@ -0,0 +1,58 @@
+package com.java.stackandqueue;
+
+/**
+ * Class to create LinkedList with different methods
+ *
+ */
+public class LinkedList {
+	Node head;
+	/*
+	 * Without LinkedList node can't exists so thats why  inside LinkedList  Class we created Node class
+	 */
+	class Node {
+		Object data;
+		Node ref;
+
+		Node(Object data) {
+			this.data = data;
+		}
+	}
+	/*Method Name:addLast
+	 * Logic for adding data for Last like
+	 * 56
+	 * 56=>30
+     * 56=>30=>70
+	 */
+
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+		Node temp = head;
+		if (head == null)
+			head = newNode;
+		else if (head.ref == null)
+			head.ref = newNode;
+		else {
+			while (temp.ref != null) {
+				temp = temp.ref;
+			}
+			temp.ref = newNode;
+		}
+	}
+	/*
+	 * Logic for displaying the added elements
+	 */
+	public void display() {
+		if (head==null) {
+			System.out.println("List is empty");
+		}
+			
+			Node temp=head;
+			while(temp != null) {
+				if(temp.ref != null)
+					System.out.print(temp.data+"=>");
+				else
+					System.out.println(temp.data);
+			   temp =temp.ref;
+		}
+	}
+}
--- src/com/java/stackandqueue/Stack.java
+++ src/com/java/stackandqueue/Stack.java
@@ -1,7 +1,35 @@
 package com.java.stackandqueue;
 
+/**
+ * Class to create Stack with different methods
+ * 
+ */
 public class Stack {
+	/*
+	 * creating the LinkedList class object
+	 */
+	LinkedList list = new LinkedList();
+
+	public void push(Object data) {
+		list.addLast(data);
+	}
+
+	public void display() {
+		list.display();
+	}
+
+	/*
+	 * Program execution starts from main method
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to the LinkedList Stack and Queue Program");
+		Stack stack = new Stack();
+		stack.push(56);
+		stack.display();
+		stack.push(30);
+		stack.display();
+		stack.push(70);
+		stack.display();
 	}
+
 }
